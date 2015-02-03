98517b0ae952c4e7fab43f694cb95be4eb03c780
Richard Ankers <rankers@deloitte.co.uk>
2015-02-03 23:38:20 +0000

    dj static

--- records/wsgi.py
+++ records/wsgi.py
@@ -27,7 +27,8 @@ os.environ.setdefault("DJANGO_CONFIGURATION", "Production")
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 from configurations.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+application = Cling(get_wsgi_application())
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
